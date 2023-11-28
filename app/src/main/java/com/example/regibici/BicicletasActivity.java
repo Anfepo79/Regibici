b53c5dbf7925c63e0c55fcb4ca7fe5089154b2e2
Nico <nizuluagaga@unal.edu.co>
2023-11-28 18:52:41 -0500

    Se añadieron los elementos faltantes de las activities finales y se empezaron a trabajar los items necesarios para hacer el cambio de pestañas entre activities.

--- app/src/main/java/com/example/regibici/BicicletasActivity.java
+++ app/src/main/java/com/example/regibici/BicicletasActivity.java
@@ -2,7 +2,10 @@ package com.example.regibici;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class BicicletasActivity extends AppCompatActivity {
 
@@ -11,4 +14,20 @@ public class BicicletasActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bicicletas);
     }
-}
+
+    //Método para cambiar boton
+    public void Siguiente(View view){
+        Intent siguiente = new Intent(this, RegistrarBiciActivity2.class);
+        startActivity(siguiente );
+    }
+
+
+
+    }
+
+
+
+
+
+
+
--- app/src/main/java/com/example/regibici/MainActivity.java
+++ app/src/main/java/com/example/regibici/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.regibici;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void SiguienteB(View view){
+        Intent siguiente = new Intent(this, RegistroActivity.class);
+        startActivity(siguiente );
+    }
+
 }
